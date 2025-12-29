fcd5fab636c46490bc3adad378bfebfacb002f55
Piotr Kubala <pkubala@student.agh.edu.pl>
2025-12-29 16:44:34 +0100

    added yaml configs

--- src/ml/learning.py
+++ src/ml/learning.py
@@ -54,6 +54,7 @@ class LearningProcess:
         num_worlds: int = 16,
         max_buffer_size: int = 1024,
         c_puct_value: float = 1.0,
+        c_puct_value: float = 1.0,
         policy_temp: float = 1.0,
         initial_max_game_length: int = 50,
         capped_max_game_length: int = 500,
